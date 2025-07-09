c136ff0f3f453d89e1b7268b23ea11d3703ecce6
Abhijeet <abhijeetw035@email.com>
2025-07-09 17:57:56 +0530

    feat: added console.error statements at required places

--- src/tracking/lib/matomo-tracking.service.ts
+++ src/tracking/lib/matomo-tracking.service.ts
@@ -129,10 +129,12 @@ export class MatomoTrackingService implements TrackingProvider {
     }
     
     if (!category || typeof category !== 'string') {
+      console.error('MatomoTrackingService: Invalid category:', category);
       return;
     }
 
     if (!action || typeof action !== 'string') {
+      console.error('MatomoTrackingService: Invalid action:', action);
       return;
     }
 
